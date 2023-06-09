4ccd510922787abfbb2409e341eea94ef712a2f1
Mateo Diet <mat.diet@orange.fr>
2023-06-09 15:54:47 +0200

    seed design v1

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :user_challenges, only: [:index, :show]
   patch "user_challenges/:id", to: "user_challenges#status", as: :change_status_user_challenge
+  resources :user_designs
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'json'
 p "clean database"
 UserChallenge.destroy_all
 Challenge.destroy_all
+UserDesign.destroy_all
 User.destroy_all
 Design.destroy_all
 
@@ -28,14 +29,18 @@ vic_design = Design.create!(
 
 p "#{Design.count} design create"
 
+victor_design = UserDesign.create!(user: victor, design: vic_design, image: File.open(Rails.root.join('app', 'assets', 'images', 'vic_design.png')), description: "")
 
-challenge = Challenge.create!(title:"Prise de décision", description:"Prendre des décisions en analysant et en prenant du temps au lieu de suivre ton intuition sur le moment", design: vic_design )
-challenge_2 = Challenge.create!(title:"Observer", description:"Observer ses propres réactions lorsque les autres ne te comprenent pas, ne t'acceptent pas ou ne suivent pas ton mouvements", design: vic_design )
-challenge_3 = Challenge.create!(title:"Se rendre compte de l'impact", description:"Faire une liste de spersonnes pouvant être impactées par tes actions et tes décisions", design: vic_design )
-challenge_4 = Challenge.create!(title:"Mode spectateur", description:"Ne pas initier et attendre que les autres fassent le premier pas ou t'invitent dans leurs oportunités", design: vic_design )
-challenge_5 = Challenge.create!(title:"Vaincre ses peurs", description:"Ne pas avoir peur de ta colère et tenter de la faire taire sans l'exprimer", design: vic_design )
-challenge_6 = Challenge.create!(title:"Réagir à ses intentions", description:"Oublier ou refuser d'informer et dissimuler tes projets ou tes intentions par peur d'être controlé.e ou empêché.e", design: vic_design )
-challenge_7 = Challenge.create!(title:"Se clarifier", description:"Clarifier ses intentions avant de se lancer", design: vic_design )
+p "#{UserDesign.count} user design create"
+
+
+challenge = Challenge.create!(title: "Prise de décision", description: "Prendre des décisions en analysant et en prenant du temps au lieu de suivre ton intuition sur le moment", design: vic_design )
+challenge_2 = Challenge.create!(title: "Observer", description: "Observer ses propres réactions lorsque les autres ne te comprenent pas, ne t'acceptent pas ou ne suivent pas ton mouvements", design: vic_design )
+challenge_3 = Challenge.create!(title: "Se rendre compte de l'impact", description: "Faire une liste de spersonnes pouvant être impactées par tes actions et tes décisions", design: vic_design )
+challenge_4 = Challenge.create!(title: "Mode spectateur", description: "Ne pas initier et attendre que les autres fassent le premier pas ou t'invitent dans leurs oportunités", design: vic_design )
+challenge_5 = Challenge.create!(title: "Vaincre ses peurs", description: "Ne pas avoir peur de ta colère et tenter de la faire taire sans l'exprimer", design: vic_design )
+challenge_6 = Challenge.create!(title: "Réagir à ses intentions", description: "Oublier ou refuser d'informer et dissimuler tes projets ou tes intentions par peur d'être controlé.e ou empêché.e", design: vic_design )
+challenge_7 = Challenge.create!(title: "Se clarifier", description: "Clarifier ses intentions avant de se lancer", design: vic_design )
 # challenge_8 = Challenge.create!(title:"Se rendre compte de l'impact", description:"Faire une liste de spersonnes pouvant être impactées par tes actions et tes décisions", design: vic_design )
 # challenge_9 = Challenge.create!(title:"Se rendre compte de l'impact", description:"Faire une liste de spersonnes pouvant être impactées par tes actions et tes décisions", design: vic_design )
 
