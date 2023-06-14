dad04e2734b0dff8372370ae4945a352231db076
victor lebret <lebret.victor02@gmail.com>
2023-06-14 12:55:14 +0200

    maj victor 146

--- app/controllers/new_designs_controller.rb
+++ app/controllers/new_designs_controller.rb
@@ -6,15 +6,15 @@ class NewDesignsController < ApplicationController
   def new_design
 
     url = "https://api.bodygraphchart.com/v221006/hd-data?api_key=#{ENV["API_KEY"]}&date=#{params[:year]}-#{params[:month]}-#{params[:day]}-#{params[:hour]}-#{params[:minutes]}"
-      design = JSON.parse(URI.open("#{url}").read)
+    design = JSON.parse(URI.open("#{url}").read)
 
 
-      strategy = design["Properties"]["Strategy"]["id"]
-      type = design["Properties"]["Type"]["id"]
-      authority = design["Properties"]["InnerAuthority"]["id"]
-      notself = design["Properties"]["NotSelfTheme"]["id"]
+    strategy = design["Properties"]["Strategy"]["id"]
+    type = design["Properties"]["Type"]["id"]
+    authority = design["Properties"]["InnerAuthority"]["id"]
+    notself = design["Properties"]["NotSelfTheme"]["id"]
 
-    design_from_api = Design.new(name_type: type, strategy: strategy, authority: authority, notself: notself )
+  design_from_api = Design.new(name_type: type, strategy: strategy, authority: authority, notself: notself )
 
     if design_from_api.save
 
