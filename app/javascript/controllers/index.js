833341872044396491c83e95e6d160a1823ea857
PierreYves Hoorens <pyhoorens@gmail.com>
2023-06-15 11:49:38 +0200

    audio

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AudiosController from "./audios_controller"
+application.register("audios", AudiosController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
