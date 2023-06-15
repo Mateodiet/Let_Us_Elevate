45cc31438f85348227c8f5d298588ad5dd6be0ad
PierreYves Hoorens <pyhoorens@gmail.com>
2023-06-15 15:14:09 +0200

    stop sound ok

--- app/javascript/controllers/audios_controller.js
+++ app/javascript/controllers/audios_controller.js
@@ -3,13 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="audios"
 export default class extends Controller {
 
-  connect() {
-    console.log("connect")
-  }
-
-  play(event) {
-    console.log("dataset",this.element.dataset);
-    const audio = new Audio(this.element.dataset.path)
-    audio.play();
+  play() {
+    // console.log("dataset", this.element.dataset);
+    if (this.audio) {
+      this.audio.pause();
+      this.audio = null;
+  } else {
+      this.audio = new Audio(this.element.dataset.path);
+      this.audio.play();
+    }
   }
 }
